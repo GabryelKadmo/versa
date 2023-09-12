f652d9d2fb0611e32d381430727e3cd7f59f96df
GabryelKadmo <gabryelkadmo@gmail.com>
2023-09-11 21:37:37 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import { useState } from 'react'
+import './App.css'
+import '../src/pages/LandingPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+  )
+}
+
+export default App
