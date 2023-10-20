eaa2f59011dc81af90252892a6bd8a0b68d0c7ae
GabryelKadmo <gabryelkadmo@gmail.com>
2023-10-20 02:33:02 -0300

    fix: resolvendo erro na vercel

--- src/pages/Novidades/NovidadesPage.tsx
+++ src/pages/Novidades/NovidadesPage.tsx
@@ -1,6 +1,5 @@
 import "./Novidades.css";
 import { Image } from "@mantine/core";
-import ImagemNovidade from "../components/images/Novidades/novidade_def_f.png";
 import { Novidade } from "../components/SessaoN/SessaoN";
 import { FiltroCima } from "../components/SessaoFc/SessaoFC";
 import { MeioFiltroProduto } from "../components/SessaoFP/SessaoFP";
