c01eb99c7439d03f365f3d28be28aaed497e6346
GabryelKadmo <gabryelkadmo@gmail.com>
2023-10-27 19:02:15 -0300

    fix: corrigindo erro do localstorage nos produtos

--- src/pages/components/CardNovidades/CardNovidades.tsx
+++ src/pages/components/CardNovidades/CardNovidades.tsx
@@ -1,73 +1,73 @@
-import { Card, Image, Text, Button, Group, Rating, Flex } from '@mantine/core';
-import "./CardNovidades.css"
-import AppContext from '../Context/AppContext';
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Card, Image, Text, Button, Group, Rating, Flex } from "@mantine/core";
+import "./CardNovidades.css";
+import AppContext from "../Context/AppContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
-    _id: string;
-    titulo: string;
-    preco: number;
-    descricao: string;
-    marca: string;
-    categoria: string;
-    rating: number;
-    avaliacao: number;
-    imgurl: string;
-}
+  _id: string;
+  titulo: string;
+  preco: number;
+  descricao: string;
+  marca: string;
+  categoria: string;
+  rating: number;
+  avaliacao: number;
+  imgurl: string;
+};
 
 export function CardNovidades(props: Props) {
+  const context = useContext(AppContext);
 
-    const context = useContext(AppContext);
+  if (context === undefined) {
+    // Trate o contexto indefinido aqui, se necessário
+    return <div>Erro: Contexto não definido.</div>;
+  }
+  const { cartItem, setCartItem } = context;
 
-    if (context === undefined) {
-        // Trate o contexto indefinido aqui, se necessário
-        return <div>Erro: Contexto não definido.</div>;
-    }
-    const { cartItem, setCartItem } = context;
+  const handleAddCart = () => {
+    setCartItem([...cartItem, props]);
+  };
 
+  localStorage.setItem("cart", JSON.stringify(cartItem));
 
-    const handleAddCart = () => {
-        setCartItem([...cartItem, props]);
-    }
-
-    localStorage.setItem('cart', JSON.stringify(cartItem));
-
-    return (
-        <div>
-            <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                    <Link to={"/produto/" + props._id}>
-                    <Image
-                        src={props.imgurl}
-                        height={330}
-                        alt="Norway"
-                    />
-                    </Link>
-                </Card.Section>
-                <Group
-                    position="apart"
-                    mt="md"
-                    mb="xs">
-                    <Text ff={'Inter'} weight={500} color='dimmed'>{props.titulo}
-                    </Text>
-                </Group>
-                <Text ff={'Inter'} size="lg" weight={800} >
-                    R${props.preco}
-                </Text>
-                <Flex gap={12} >
-                    <Rating pt={6} defaultValue={props.avaliacao} fractions={2} readOnly />
-                    <Text pt={3} color="dimmed">({props.avaliacao})</Text>
-                </Flex>
-                <Button
-                    color="dark"
-                    fullWidth
-                    mt="md"
-                    radius="lg"
-                    onClick={handleAddCart}>
-                    Adicionar ao carrinho
-                </Button>
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section>
+          <Link to={"/produto/" + props._id}>
+            <Image src={props.imgurl} height={330} alt="Norway" />
+          </Link>
+        </Card.Section>
+        <Group position="apart" mt="md" mb="xs">
+          <Text ff={"Inter"} weight={500} color="dimmed">
+            {props.titulo}
+          </Text>
+        </Group>
+        <Text ff={"Inter"} size="lg" weight={800}>
+          R${props.preco}
+        </Text>
+        <Flex gap={12}>
+          <Rating
+            pt={6}
+            defaultValue={props.avaliacao}
+            fractions={2}
+            readOnly
+          />
+          <Text pt={3} color="dimmed">
+            ({props.avaliacao})
+          </Text>
+        </Flex>
+        <Button
+          color="dark"
+          fullWidth
+          mt="md"
+          radius="lg"
+          onClick={handleAddCart}
+        >
+          Adicionar ao carrinho
+        </Button>
+      </Card>
+    </div>
+  );
 }
--- src/pages/components/InfoProduto/BotaoCompra.tsx
+++ src/pages/components/InfoProduto/BotaoCompra.tsx
@@ -1,46 +1,54 @@
-import "./BotaoCompra.css"
-import { Button } from '@mantine/core';
+import "./BotaoCompra.css";
+import { Button } from "@mantine/core";
 import { IconShoppingCart } from "@tabler/icons-react";
 import { useContext } from "react";
 import AppContext from "../Context/AppContext";
 
 type Props = {
-    _id: string;
-    titulo: string;
-    preco: number;
-    descricao: string;
-    marca: string;
-    categoria: string;
-    rating: number;
-    avaliacao: number;
-    imgurl: string;
-}
+  _id: string;
+  titulo: string;
+  preco: number;
+  descricao: string;
+  marca: string;
+  categoria: string;
+  rating: number;
+  avaliacao: number;
+  imgurl: string;
+};
 
-export function BotaoCompra(props: Props){
+export function BotaoCompra(props: Props) {
+  const context = useContext(AppContext);
 
-    const context = useContext(AppContext);
+  if (context === undefined) {
+    // Trate o contexto indefinido aqui, se necessário
+    return <div>Erro: Contexto não definido.</div>;
+  }
 
-    if (context === undefined) {
-        // Trate o contexto indefinido aqui, se necessário
-        return <div>Erro: Contexto não definido.</div>;
+  const { cartItem, setCartItem } = context;
+
+  const handleAddCart = () => {
+    console.log(cartItem, props);
+
+    if (props._id !== undefined) {
+      setCartItem([...cartItem, props]);
+      console.log(cartItem, props);
     }
+  };
+  localStorage.setItem("cart", JSON.stringify(cartItem));
 
-    const { cartItem, setCartItem } = context;
-
-    const handleAddCart = () => {
-        if(props._id !== undefined){
-            localStorage.setItem('cart', JSON.stringify(cartItem))
-            setCartItem([...cartItem, props]);
-            console.log(cartItem, props)
-        }
-    }    
-        
-
-    return(
-        <div>
-            <Button onClick={handleAddCart} fw={600} color="dark" radius={"xl"} leftIcon={<IconShoppingCart size="1rem" />} w={230} h={42}>
-                Adicionar ao carrinho
-            </Button>
-        </div>
-    )
+  return (
+    <div>
+      <Button
+        onClick={handleAddCart}
+        fw={600}
+        color="dark"
+        radius={"xl"}
+        leftIcon={<IconShoppingCart size="1rem" />}
+        w={230}
+        h={42}
+      >
+        Adicionar ao carrinho
+      </Button>
+    </div>
+  );
 }
